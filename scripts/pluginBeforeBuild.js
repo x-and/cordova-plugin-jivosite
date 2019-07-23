657b8d8be41d0c532289c0027c4c55916486e3b6
Андреянов Александр Николаевич <a.andreyanov@sevstar.net>
2019-07-23 10:05:21 +0300

    fixed type in script name, add jivosite-conf.json to plugin

--- scripts/pluginBeforeBuild.js
+++ scripts/pluginBeforeBuild.js
@@ -4,19 +4,21 @@ const path = require('path');
 
 module.exports = function(ctx) {
 
-    const jivosite = path.join(ctx.opts.projectRoot, 'platforms/android/app/src/main/assets/jivosite');
-    const filename = 'jivosite-conf.json';
-    const pathToFile = path.join(ctx.opts.projectRoot, filename);
+    const filenameFrom = 'jivosite-conf.json';
+    const filenameTo = 'jivosite-conf.js';
+
+    const pathToJivosite = path.join(path.join(ctx.opts.projectRoot, 'platforms/android/app/src/main/assets/jivosite'), filenameTo);
+    const pathToFile = path.join(ctx.opts.projectRoot, filenameFrom);
 
     if (!fs.existsSync(pathToFile)) {
     	throw new Exception('no jivosite-conf.json found in root')
     }
 
-	fs.copyFileSync(pathToFile, path.join(jivosite, filename));
+	fs.copyFileSync(pathToFile, pathToJivosite);
 
 	/* make valid js file from json */
-	const data = fs.readFileSync(path.join(jivosite, filename))
-	const fd = fs.openSync(path.join(jivosite, filename), 'w+')
+	const data = fs.readFileSync(pathToJivosite)
+	const fd = fs.openSync(pathToJivosite, 'w+')
 	const insert = new Buffer("jivo_config = ")
 	fs.writeSync(fd, insert, 0, insert.length, 0)
 	fs.writeSync(fd, data, 0, data.length, insert.length)
