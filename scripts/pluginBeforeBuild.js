170cf4b6a10fb56044affcb0574ff84d382def95
Андреянов Александр Николаевич <a.andreyanov@sevstar.net>
2019-12-18 15:14:29 +0300

    beforeBuild script fixes

--- scripts/pluginBeforeBuild.js
+++ scripts/pluginBeforeBuild.js
@@ -7,7 +7,8 @@ module.exports = function(ctx) {
     const filenameFrom = 'jivosite-conf.json';
     const filenameTo = 'jivosite-conf.js';
 
-    const pathToJivosite = path.join(path.join(ctx.opts.projectRoot, 'platforms/android/app/src/main/assets/jivosite'), filenameTo);
+    const pathToJivosite = path.join(ctx.opts.projectRoot, 'platforms/android/app/src/main/assets/jivosite');
+    const pathJivositeFile = path.join(pathToJivosite, filenameTo);
     const pathToFile = path.join(ctx.opts.projectRoot, filenameFrom);
 
     try {
@@ -16,20 +17,20 @@ module.exports = function(ctx) {
 
 
     if (!fs.existsSync(pathToFile)) {
-    	throw new Exception('no jivosite-conf.json found in root')
+        throw new Exception('no jivosite-conf.json found in root')
     }
 
-	fs.copyFileSync(pathToFile, pathToJivosite);
+    fs.copyFileSync(pathToFile, pathJivositeFile);
 
-	/* make valid js file from json */
-	const data = fs.readFileSync(pathToJivosite)
-	const fd = fs.openSync(pathToJivosite, 'w+')
-	const insert = new Buffer("jivo_config = ")
-	fs.writeSync(fd, insert, 0, insert.length, 0)
-	fs.writeSync(fd, data, 0, data.length, insert.length)
-	fs.close(fd, (err) => {
-		if (err) throw err;
-	});
+    /* make valid js file from json */
+    const data = fs.readFileSync(pathJivositeFile)
+    const fd = fs.openSync(pathJivositeFile, 'w+')
+    const insert = new Buffer("jivo_config = ")
+    fs.writeSync(fd, insert, 0, insert.length, 0)
+    fs.writeSync(fd, data, 0, data.length, insert.length)
+    fs.close(fd, (err) => {
+        if (err) throw err;
+    });
 
     const plugin = path.join(ctx.opts.plugin.dir, 'www');
 
